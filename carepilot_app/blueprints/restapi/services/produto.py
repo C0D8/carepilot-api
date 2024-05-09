14acdb3e119e8d9f99025580a2a201933da840dc
Juliakp1 <juliakp710@gmail.com>
2024-05-09 08:07:37 -0300

    added product routes

--- carepilot_app/blueprints/restapi/services/produto.py
+++ carepilot_app/blueprints/restapi/services/produto.py
@@ -1,10 +1,15 @@
 from carepilot_app.extensions.db import db #noqa
 from carepilot_app.models.produto import Produto
 from carepilot_app.schemas.produto import ProdutoSchema
+from carepilot_app.models.movimento import Movimento
+from carepilot_app.schemas.movimento import MovimentoSchema
+
+import pandas as pd
 
 list_produtos = ProdutoSchema(many=True)
 produto_schema = ProdutoSchema()
-
+movimento_schema = MovimentoSchema()
+list_movimentos = MovimentoSchema(many=True)
 
 def get_produtos():
     produtos = Produto.find_all()
@@ -36,8 +41,6 @@ def update_produto(produto_id, data):
     produto.update_to_db()
     return produto_schema.dump(produto), 200
 
-    
-    
 
 def delete_produto(produto_id):
     produto = Produto.find_by_id(produto_id)
@@ -48,3 +51,32 @@ def delete_produto(produto_id):
     produto.delete_from_db()
     return {"message": "Produto deleted"}, 200
 
+def product_cliente(product_id):
+
+    movimentos = Movimento.find_all()
+    movimentos = pd.DataFrame([movimento.json() for movimento in movimentos])
+
+    df_produto = movimentos.groupby(["cliente_id", "produto_id"]).agg({'quantidade': 'sum'}).reset_index()
+    content = df_produto[df_produto["cod_produto"] == product_id]
+    content = content.drop("cod_produto", axis=1)
+    content = content.sort_values("quantidade", ascending=False)
+    content = content.head(5)
+    content.columns = ["Cliente", "Quantidade"]
+
+    return content.to_dict(orient='records')
+
+def product_grafico(product_id):
+
+    movimentos = Movimento.find_all()
+    movimentos = pd.DataFrame([movimento.json() for movimento in movimentos])
+    
+    content = movimentos[movimentos["cod_produto"] == product_id]
+    content = content.drop("cod_produto", axis=1)
+    content["data_documento"] = pd.to_datetime(content["data_documento"], format="%d/%m/%Y %H:%M:%S")
+
+    # Agrupar por semana
+    content.set_index("data_documento", inplace=True)
+    content = content.resample('W').sum()
+    content = content.reset_index()
+
+    return content.to_dict(orient='records')
